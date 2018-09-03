be0b4e729ee9c57b1c2ed145871a0009836cbf9d
houssaink <houssaink@gmail.com>
2018-09-03 12:24:17 +0200

    add the project entity

---
+++ TRS_Application_EJB/ejbModule/be/iris/entities/Project.java
@@ -0,0 +1,19 @@
+package be.iris.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Project {
+
+	private String ptitle;
+
+	public String getPtitle() {
+		return ptitle;
+	}
+
+	public void setPtitle(String ptitle) {
+		this.ptitle = ptitle;
+	}
+	
+	
+}
