cd6a948cb9586a2601ae946a0b53d0b80ee4f2c8
houssaink <houssaink@gmail.com>
2018-09-04 11:59:01 +0200

    Change implementiation to check
    PersonDao Implementation to get the persons with a password

--- TRS_Application_EJB/ejbModule/be/iris/entities/Tutperson.java
+++ TRS_Application_EJB/ejbModule/be/iris/entities/Tutperson.java
@@ -28,7 +28,7 @@ import javax.validation.constraints.NotNull;
 @Named
 @Entity
 @Table(name="TUTPERSONS")
-@NamedQuery(name="Tutperson.findAll", query="SELECT t FROM Tutperson t")
+@NamedQuery(name="Tutperson.findAll", query="SELECT t FROM Tutperson t ")
 public class Tutperson implements Serializable {
 	private static final long serialVersionUID = 1L;
 
