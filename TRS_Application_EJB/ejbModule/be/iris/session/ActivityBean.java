cbbcff83ba4fd03c131070adbe5dce861b955b89
Giel Blockx <gielblockx2@hotmail.com>
2018-09-04 10:30:15 +0200

    Activites bean
    Done
    
    Signed-off-by: Giel Blockx <gielblockx2@hotmail.com>

--- TRS_Application_EJB/ejbModule/be/iris/session/ActivityBean.java
+++ TRS_Application_EJB/ejbModule/be/iris/session/ActivityBean.java
@@ -1,17 +1,131 @@
 package be.iris.session;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.ejb.Stateless;
 
 import be.iris.entities.Activity;
+import be.iris.entities.Tutperson;
+import be.iris.entities.Tutproject;
 import be.iris.session.view.ActivityBeanRemote;
 
 @Stateless(mappedName = "activityBean")
 public class ActivityBean implements ActivityBeanRemote {
 
-	
+	// need jpa controller for it
 	
     public ActivityBean() {
     }
+
+	@Override
+	public void saveNewActivitie(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Activity> getAllActivities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOnDate(LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfPerson(Tutperson person) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfPersonAtDate(Tutperson person, LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfProject(Tutproject Project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfProjectAtDate(Tutproject project, LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfPersonWorkingAtProject(Tutperson person, Tutproject project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getAllActivitiesOfPersonWorkingAtProjectOnDate(Tutperson person, Tutproject project,
+			LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateListOfActivities(List<Activity> activities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteListOfActivities(List<Activity> activities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivitiesOnDate(LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivitiesOfPerson(Tutperson person) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivitiesOfPersonAtDate(Tutperson person, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivitiesOfProject(Tutproject project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivitiesOfProjectAtDate(Tutproject project, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
     
     
 
--- TRS_Application_EJB/ejbModule/be/iris/session/view/ActivityBeanRemote.java
+++ TRS_Application_EJB/ejbModule/be/iris/session/view/ActivityBeanRemote.java
@@ -6,20 +6,32 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import be.iris.entities.Activity;
+import be.iris.entities.Tutperson;
+import be.iris.entities.Tutproject;
 
 @Remote
 public interface ActivityBeanRemote {
 	
 	public void saveNewActivitie(Activity activity);
 	
+	public void updateActivity(Activity activity);
+	public void updateListOfActivities(List<Activity> activities);
+	
 	public List<Activity> getAllActivities();
-	public List<Activity> getAllActivitiesOfPerson(Person person);
-	public List<Activity> getAllActivitesOfPersonAtDate(Person person, LocalDate date);
-	public List<Activity> getAllActivitiesOfProject(Project Project);
-	public List<Activity> getAllActivitiesOfProjectAtDate(Project project, LocalDate date);
-	public List<Activity> getAllActivitiesOfPersonWorkingAtProject();
-	public List<Activity> getAllActivitiesOfPersonWorkingAtProjectOnDate();
+	public List<Activity> getAllActivitiesOnDate(LocalDate date);
+	public List<Activity> getAllActivitiesOfPerson(Tutperson person);
+	public List<Activity> getAllActivitiesOfPersonAtDate(Tutperson person, LocalDate date);
+	public List<Activity> getAllActivitiesOfProject(Tutproject Project);
+	public List<Activity> getAllActivitiesOfProjectAtDate(Tutproject project, LocalDate date);
+	public List<Activity> getAllActivitiesOfPersonWorkingAtProject(Tutperson person, Tutproject project);
+	public List<Activity> getAllActivitiesOfPersonWorkingAtProjectOnDate(Tutperson person, Tutproject project, LocalDate date);
 	
+	public void deleteActivity(Activity activity);
+	public void deleteListOfActivities(List<Activity> activities);
+	public void deleteActivitiesOnDate(LocalDate date);
+	public void deleteActivitiesOfPerson(Tutperson person);
+	public void deleteActivitiesOfPersonAtDate(Tutperson person, LocalDate date);
+	public void deleteActivitiesOfProject(Tutproject project);
+	public void deleteActivitiesOfProjectAtDate(Tutproject project, LocalDate date);
 	
-
 }
