328f462c269086e4f85370f61f3485f03b018989
gielb <gielblockx2@hotmail.com>
2018-09-05 04:59:41 +0200

    added project bean
    added project bean interface and impl
    
    Signed-off-by: gielb <gielblockx2@hotmail.com>

---
+++ TRS_Application_EJB/ejbModule/be/iris/session/ProjectBean.java
@@ -0,0 +1,61 @@
+package be.iris.session;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import be.iris.dao.ProjectDao;
+import be.iris.entities.Tutcours;
+import be.iris.entities.Tutproject;
+import be.iris.session.view.ProjectBeanRemote;
+
+@Stateless(mappedName = "projectBean")
+public class ProjectBean implements ProjectBeanRemote {
+
+	@EJB(name = "projectImpl")
+	private ProjectDao projectDao;
+
+	public ProjectBean() {
+	}
+
+	@Override
+	public void saveNewProject(Tutproject project) {
+		projectDao.insertProject(project);
+	}
+
+	@Override
+	public void changeProject(Tutproject oldProject, Tutproject newProject) {
+		projectDao.updateProject(oldProject, newProject);
+	}
+
+	@Override
+	public void removeProject(Tutproject project) {
+		projectDao.deleteProject(project);
+	}
+
+	@Override
+	public Tutproject getProject(Tutproject project) {
+		return projectDao.getProject(project);
+	}
+
+	@Override
+	public List<Tutproject> getAllPrjects() {
+		return projectDao.listAllprojects();
+	}
+
+	// needs more impl
+	// don't know meaning
+	@Override
+	public List<Tutcours> getListInvoicedProject() {
+		return projectDao.listInvoicedProjects();
+	}
+
+	// needs more impl
+	// don't know meaning
+	@Override
+	public List<Tutproject> getListNonInvoicesProjects() {
+		return projectDao.listAllNonInvoicedProjects();
+	}
+
+}
---
+++ TRS_Application_EJB/ejbModule/be/iris/session/view/ProjectBeanRemote.java
@@ -0,0 +1,29 @@
+package be.iris.session.view;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import be.iris.entities.Tutcours;
+import be.iris.entities.Tutproject;
+
+@Remote
+public interface ProjectBeanRemote {
+
+	public void saveNewProject(Tutproject project);
+
+	public void changeProject(Tutproject oldProject, Tutproject newProject);
+
+	public void removeProject(Tutproject project);
+	
+	public Tutproject getProject(Tutproject project);
+
+	public List<Tutproject> getAllPrjects();
+
+	// i don't know
+	public List<Tutcours> getListInvoicedProject();
+
+	// i don't know
+	public List<Tutproject> getListNonInvoicesProjects();
+
+}
