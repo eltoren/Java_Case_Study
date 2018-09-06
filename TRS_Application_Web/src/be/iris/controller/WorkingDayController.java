a8477fb05557049b26ec5b9f9bd5d3dcb5d99542
Giel Blockx <gielblockx2@hotmail.com>
2018-09-06 16:25:45 +0200

    adding workingday function
    Signed-off-by: Giel Blockx <gielblockx2@hotmail.com>

--- TRS_Application_Web/src/be/iris/controller/WorkingDayController.java
+++ TRS_Application_Web/src/be/iris/controller/WorkingDayController.java
@@ -1,10 +1,40 @@
 package be.iris.controller;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import be.iris.session.view.WorkingDayBeanRemote;
+
 @Named
-@RequestScoped
+@SessionScoped
 public class WorkingDayController {
+	
+	@EJB(name = "workingDayBean")
+	private WorkingDayBeanRemote workingDayBean;
+	
+	@Inject
+	private LoginController loginController;
+	
+	public void startWorkignDay() {
+		System.out.println(loginController.getPersonSelected());
+		workingDayBean.StartNewWorkingDay(loginController.getPersonSelected());
+	}
+	
+	public void endWorkingDay() {
+		System.out.println(loginController.getPersonSelected());
+		workingDayBean.endWorkingDay(loginController.getPersonSelected());
+	}
+
+	public LoginController getLoginController() {
+		return loginController;
+	}
+
+	public void setLoginController(LoginController loginController) {
+		this.loginController = loginController;
+	}
+	
 
 }
