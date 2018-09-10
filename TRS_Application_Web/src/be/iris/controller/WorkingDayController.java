5b3b863618dbf5323c6d84a37d10e0df2cdaa8d8
Giel Blockx <gielblockx2@hotmail.com>
2018-09-10 16:41:27 +0200

    Merge branch 'master' of git@github.com:eltoren/Java_Case_Study.git
    # Conflicts:
    #	TRS_Application_Web/WebContent/MainPage.xhtml
    #	TRS_Application_Web/src/be/iris/controller/WorkingDayController.java
    
    Signed-off-by: Giel Blockx <gielblockx2@hotmail.com>

--- TRS_Application_Web/src/be/iris/controller/WorkingDayController.java
+++ TRS_Application_Web/src/be/iris/controller/WorkingDayController.java
@@ -12,31 +12,6 @@ import be.iris.session.view.WorkingDayBeanRemote;
 @SessionScoped
 public class WorkingDayController {
 	
-	@EJB(name = "workingDayBean")
-	private WorkingDayBeanRemote workingDayBean;
-	
-	@Inject
-	private LoginController loginController;
-	
-	public void startWorkignDay() {
-		System.out.println(loginController.getPersonSelected());
-		workingDayBean.StartNewWorkingDay(loginController.getPersonSelected());
-	}
-	
-	public void endWorkingDay() {
-		System.out.println(loginController.getPersonSelected());
-		workingDayBean.endWorkingDay(loginController.getPersonSelected());
-	}
-
-	public LoginController getLoginController() {
-		return loginController;
-	}
-
-	public void setLoginController(LoginController loginController) {
-		this.loginController = loginController;
-	}
-	
-
 	@Inject
 	private LoginController loginController;
 
